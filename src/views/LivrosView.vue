12fe6b387ec1ff8f9bca0a50cd5bab5e1085cccb
José Henrique Cabianchi Orlandi <joseorlandi13@gmail.com>
2024-01-17 15:01:16 -0300

    Ajuste na parte visual do CRUD de livros

--- src/views/LivrosView.vue
+++ src/views/LivrosView.vue
@@ -21,18 +21,35 @@
               </div>
               <v-text-field
                 v-model="nome_livro"
-                :rules="rules"
+                :rules="divAtiva === 'cadastrar' ? rules : []"
                 label="Nome do Livro"
               ></v-text-field>
               <v-btn type="submit" block class="mt-2">Cadastrar</v-btn>
-              <v-alert v-if="showSuccess" type="success" class="mt-2">
-                Cadastro realizado com sucesso!
-              </v-alert>
             </v-form>
           </v-sheet>
         </div>
       </v-col>
     </v-row>
+
+    <v-row v-if="modoEdicao" align="center" justify="center">
+      <v-col>
+        <v-sheet width="300" class="mx-auto">
+          <v-form @submit.prevent="salvarEdicao">
+            <div class="mb-2 text-center">
+              <h1>
+                Editar Livro
+              </h1>
+            </div>
+            <v-text-field
+              v-model="livroEditado.nome_livro"
+              label="Nome do Livro"
+            ></v-text-field>
+            <v-btn type="submit" block class="mt-2">Salvar</v-btn>
+          </v-form>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    
     <v-row>
       <v-col>
         <div v-show="divAtiva === 'listar'" class="tableListar">
@@ -49,6 +66,15 @@
         </div>
       </v-col>
     </v-row>
+    <v-alert v-if="showSuccess" type="success" class="mt-2">
+      Cadastro realizado com sucesso!
+    </v-alert>
+    <v-alert v-if="deleteSucess" type="success" class="mt-2">
+      Livro excluido com sucesso.
+    </v-alert>
+    <v-alert v-if="deleteError" type="error" class="mt-2">
+      Erro ao excluir o Livro.
+    </v-alert>
   </div>
 </template>
 
@@ -77,10 +103,9 @@ import moment from 'moment'
 export default {
   data: () => ({
     nome_livro: '',
-    rules: [
-      v => !!v || 'Nome do livro é necessário.',
-    ],
     showSuccess: false,
+    deleteSucess: false,
+    deleteError: false,
     divAtiva: 'listar',
     livros: [],
     headers: [
@@ -90,31 +115,41 @@ export default {
       { text: 'Data da Alteração', value: 'dt_alteracao' },
       { text: 'Ação', value: 'action', sortable: false },
     ],
+    modoEdicao: false,
+    livroEditado: {
+      id: null,
+      nome_livro: '',
+      dt_inclusao: '',
+      dt_alteracao: '',
+    },
   }),
   methods: {
     mostrar(div) {
+      if (div === 'cadastrar') {
+        this.nome_livro = ''
+      }
       this.divAtiva = div
     },
 
     async fetchLivros() {
-      const url = 'http://localhost:8000/api/livros';
+      const url = 'http://localhost:8000/api/livros'
       try {
-        const response = await fetch(url);
+        const response = await fetch(url)
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
 
-        const data = await response.json();
+        const data = await response.json()
 
         // Formate as datas usando moment
         data.livros.forEach(livro => {
-          livro.dt_inclusao = moment(livro.dt_inclusao).format('DD/MM/YYYY');
-          livro.dt_alteracao = moment(livro.dt_alteracao).format('DD/MM/YYYY');
-        });
+          livro.dt_inclusao = moment(livro.dt_inclusao).format('DD/MM/YYYY')
+          livro.dt_alteracao = moment(livro.dt_alteracao).format('DD/MM/YYYY')
+        })
 
         this.livros = data.livros;
       } catch (error) {
-        console.error('Error fetching livro data:', error.message);
+        console.error('Error fetching livro data:', error.message)
       }
     },
 
@@ -134,34 +169,79 @@ export default {
         });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
 
-        this.showSuccess = true;
-
-        // Log para verificar se o novo livro é retornado corretamente
-        const newLivro = await response.json();
-        console.log('Novo Livro:', newLivro);
-
-        // Atualize a tabela após o sucesso do cadastro
-        this.fetchLivros();
+        this.showSuccess = true
+        this.fetchLivros()
 
         setTimeout(() => {
-          this.showSuccess = false;
+          this.showSuccess = false
         }, 3000);
+        this.divAtiva = 'listar'
       } catch (error) {
-        console.error('Houve um problema com a solicitação Fetch:', error.message);
+        console.error('Houve um problema com a solicitação Fetch:', error.message)
       }
     },
 
-    editarLivro(item) {
+    async editarLivro(item) {
+      this.livroEditado = {
+        id: item.id,
+        nome_livro: item.nome_livro,
+        dt_inclusao: item.dt_inclusao,
+        dt_alteracao: item.dt_alteracao,
+      }
+      this.divAtiva = 'editar'
+      this.modoEdicao = true
     },
 
-    excluirLivro(id) {
+    async salvarEdicao() {
+      try {
+        const response = await fetch(`http://localhost:8000/api/livros/${this.livroEditado.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            nome_livro: this.livroEditado.nome_livro,
+          }),
+        })
+
+        if (response.ok) {
+          this.modoEdicao = false
+          this.divAtiva = 'listar'
+          this.fetchLivros()
+        } else {
+          console.error('Erro ao editar o livro')
+        }
+      } catch (error) {
+        console.error('Erro na requisição de edição:', error.message)
+      }
+    },
+
+    async excluirLivro(id) {
+      try {
+        const response = await fetch(`http://localhost:8000/api/livros/${id}`, {
+          method: 'DELETE',
+        });
+
+        if (response.ok) {
+          this.fetchLivros()
+          this.deleteSucess = true
+        } else {
+          this.deleteError = true
+        }
+        setTimeout(() => {
+          this.deleteSucess = false
+          this.deleteError = false
+        }, 3000);
+      } catch (error) {
+        console.error('Erro na requisição de exclusão:', error.message)
+      }
     },
   },
   created() {
-    this.fetchLivros();
+    this.fetchLivros()
   },
 }
 </script>
